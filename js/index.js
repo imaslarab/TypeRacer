d3dee729f7120ec1392a1b00055ddb0024f56db8
imaslarab <baralsami@gmail.com>
2016-09-26 17:51:23 +0545

    Moving car added

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 function Racer() {
 
 	var SPACEBARKEY = 32;
-	var paragraph;
+
 	var inputArea;
 	var textArea;
+	var racingArea;
+
+	var car;
+	var carWidth;
+	var containerWidth;
+	var paragraph;
 	var text;
 	var inputText;
 	var words;
@@ -12,15 +18,19 @@ function Racer() {
 	this.init = function() {
 		textArea = document.getElementById('text-area');
 		inputArea = document.getElementById('input-area');
+		racingArea = document.getElementById('racing-area');
 
-		text = 'Keying text is important, but it is more important to be able to make it look professional. Keying text is important, but it is more important to be able to make it look professional.';
+		car = document.createElement('div');
+		car.setAttribute('class', 'car');
+		racingArea.appendChild(car);
+
+		containerWidth = parseInt(window.getComputedStyle(car.parentNode, null).getPropertyValue('width'));
+		carWidth = parseInt(window.getComputedStyle(car, null).getPropertyValue('width'));
+
+		text = 'Keying text is important, but it is more important to be able to make it look professional.';
 		words = text.split(' ');
 
-		for(var j = 0; j < words.length; j++) {
-			paragraph = document.createElement('span');
-			paragraph.innerHTML += words[j] + " "; 
-			textArea.appendChild(paragraph);
-		}
+		addSpan();
 		updatePlayGround(0);
 
 		inputText = document.createElement('input');
@@ -37,14 +47,25 @@ function Racer() {
 		});
 	}
 
+	function addSpan() {
+		for(var j = 0; j < words.length; j++) {
+			paragraph = document.createElement('span');
+			paragraph.innerHTML += words[j] + " "; 
+			textArea.appendChild(paragraph);
+		}
+	}
+
 	function match(inputValue, givenValue) {
 		
 		if(inputValue == givenValue) {
 			inputText.value = '';
 			currentIndex ++;
+			moveCar(currentIndex);
 			updatePlayGround(currentIndex);
-			if( currentIndex == words.length) {
-				alert("You Win");
+			if( currentIndex >= words.length) {
+				setTimeout(function() {
+					alert("You Win");
+            	},500);
 			}
 		}
 
@@ -64,6 +85,10 @@ function Racer() {
 				textArea.children[j].setAttribute('class','');
 			}
 		}
+	}
+
+	function moveCar(currentIndex) {
+		car.style.left = (currentIndex / words.length) * (100 - (carWidth/containerWidth * 100)) + '%';
 	}
 
 }
